261dbcc3abfc6615945f1227a29614271d9aafad
kraynel <kraynel@yahoo.fr>
2017-12-05 20:37:45 +0100

    Add day5, part 2

--- src/day5.js
+++ src/day5.js
@@ -12,6 +12,26 @@ const getNumberOfSteps = input => {
   return accu;
 };
 
+const getNumberOfSteps2 = input => {
+  const steps = input.split(/\n/).map(v => parseInt(v));
+
+  let cursor = 0;
+  let accu = 0;
+  while (cursor < steps.length && cursor >= 0) {
+    const newCursor = cursor + steps[cursor];
+    if (steps[cursor] >= 3) {
+      steps[cursor]--;
+    } else {
+      steps[cursor]++;
+    }
+
+    cursor = newCursor;
+    accu++;
+  }
+  return accu;
+};
+
 module.exports = {
-  getNumberOfSteps
+  getNumberOfSteps,
+  getNumberOfSteps2
 };
--- src/day5.test.js
+++ src/day5.test.js
@@ -1,4 +1,4 @@
-const { getNumberOfSteps } = require("./day5");
+const { getNumberOfSteps, getNumberOfSteps2 } = require("./day5");
 const fs = require("fs");
 
 describe("getNumberOfSteps", () => {
@@ -15,6 +15,25 @@ describe("getNumberOfSteps", () => {
   it("should log the correct value", done => {
     fs.readFile("inputs/day5.txt", "utf8", (err, data) => {
       expect(getNumberOfSteps(data)).toMatchSnapshot();
+      done();
+    });
+  });
+});
+
+describe("getNumberOfSteps2", () => {
+  it("should check if is valid 1", () => {
+    expect(
+      getNumberOfSteps2(`0
+  3
+  0
+  1
+  -3`)
+    ).toBe(10);
+  });
+
+  it("should log the correct value", done => {
+    fs.readFile("inputs/day5.txt", "utf8", (err, data) => {
+      expect(getNumberOfSteps2(data)).toMatchSnapshot();
       done();
     });
   });
