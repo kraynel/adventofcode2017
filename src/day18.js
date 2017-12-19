e61163655fc19a8082f8de69a33bf49f4c708068
kraynel <kraynel@yahoo.fr>
2017-12-19 19:59:39 +0100

    Add day 18 part 2

--- src/day18.js
+++ src/day18.js
@@ -57,4 +57,102 @@ const executeProgram = input => {
   return;
 };
 
-module.exports = { executeProgram };
+const executeProgramWithSend = (
+  instructions,
+  registers,
+  pointer = 0,
+  sendBuffer = [],
+  receiveBuffer = []
+) => {
+  while (pointer >= 0 && pointer < instructions.length) {
+    const nextInstruction = instructions[pointer];
+    switch (nextInstruction[0]) {
+      case "snd":
+        registers.sendValue++;
+        sendBuffer.push(getValue(registers, nextInstruction[1]));
+        pointer++;
+        break;
+      case "set":
+        registers[nextInstruction[1]] = getValue(registers, nextInstruction[2]);
+        pointer++;
+        break;
+      case "add":
+        registers[nextInstruction[1]] += getValue(
+          registers,
+          nextInstruction[2]
+        );
+        pointer++;
+        break;
+      case "mul":
+        registers[nextInstruction[1]] *= getValue(
+          registers,
+          nextInstruction[2]
+        );
+        pointer++;
+        break;
+      case "mod":
+        registers[nextInstruction[1]] %= getValue(
+          registers,
+          nextInstruction[2]
+        );
+        pointer++;
+        break;
+      case "rcv":
+        if (receiveBuffer.length != 0) {
+          registers[nextInstruction[1]] = receiveBuffer.shift();
+          pointer++;
+        } else {
+          return {
+            registers,
+            pointer,
+            sendBuffer,
+            receiveBuffer,
+            isWaiting: true
+          };
+        }
+        break;
+      case "jgz":
+        if (getValue(registers, nextInstruction[1]) > 0) {
+          pointer += getValue(registers, nextInstruction[2]);
+        } else pointer++;
+        break;
+    }
+  }
+  return;
+};
+
+const executeInParallel = input => {
+  const instructions = input.split(/\n/).map(exec => exec.split(" "));
+  let registers = [{ p: 0, sendValue: 0 }, { p: 1, sendValue: 0 }];
+  let pointers = [0, 0];
+  let queues = [[], []];
+
+  let index = 0;
+  while (true) {
+    if (_.every(registers, "skipped")) break;
+
+    const nextInstruction = instructions[pointers[index]];
+    if (nextInstruction[0] === "rcv" && _.isEmpty(queues[(index + 1) % 2])) {
+      registers[index].skipped = true;
+    } else {
+      registers[index].skipped = false;
+      let results = executeProgramWithSend(
+        instructions,
+        registers[index],
+        pointers[index],
+        queues[index],
+        queues[(index + 1) % 2]
+      );
+      if (!results.isWaiting) {
+        console.log("Program", index, "has terminated");
+      }
+      pointers[index] = results.pointer;
+    }
+
+    index = (index + 1) % 2;
+  }
+
+  return registers[1].sendValue;
+};
+
+module.exports = { executeProgram, executeInParallel };
--- src/day18.test.js
+++ src/day18.test.js
@@ -1,4 +1,4 @@
-const { executeProgram } = require("./day18");
+const { executeProgram, executeInParallel } = require("./day18");
 const fs = require("fs");
 
 describe("executeProgram", () => {
@@ -20,6 +20,27 @@ jgz a -2`)
   it("should get the value", done => {
     fs.readFile("inputs/day18.txt", "utf8", (err, data) => {
       expect(executeProgram(data)).toMatchSnapshot();
+      done();
+    });
+  });
+});
+
+describe("executeProgram", () => {
+  it("should get the transmitted value", () => {
+    expect(
+      executeInParallel(`snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d`)
+    ).toBe(3);
+  });
+
+  it("should get the value", done => {
+    fs.readFile("inputs/day18.txt", "utf8", (err, data) => {
+      expect(executeInParallel(data)).toMatchSnapshot();
       done();
     });
   });
