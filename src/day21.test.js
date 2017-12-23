fd3615941936037dbe040068b6e8fa3cc0ba65b6
Kevin Raynel <kevinr@theodo.fr>
2017-12-23 13:33:46 +0100

    Add part 2, day 21

--- src/day21.test.js
+++ src/day21.test.js
@@ -2,24 +2,24 @@ const { initialGrid, getNextGrid, parseInput } = require('./day21');
 const fs = require('fs');
 
 describe('getNextGrid', () => {
-  //   it('should getNextGrid', () => {
-  //     const transformations = parseInput(`../.# => ##./#../...
-  // .#./..#/### => #..#/..../..../#..#`);
-  //     expect(getNextGrid(initialGrid, transformations)).toEqual([
-  //       '#..#',
-  //       '....',
-  //       '....',
-  //       '#..#'
-  //     ]);
-  //   });
+  it('should getNextGrid', () => {
+    const transformations = parseInput(`../.# => ##./#../...
+.#./..#/### => #..#/..../..../#..#`);
+    expect(getNextGrid(initialGrid, transformations)).toEqual([
+      '#..#',
+      '....',
+      '....',
+      '#..#'
+    ]);
+  });
 
-  //   it('should getNextGrid', () => {
-  //     const transformations = parseInput(`../.# => ##./#../...
-  // .#./..#/### => #..#/..../..../#..#`);
-  //     expect(
-  //       getNextGrid(['....', '.#.#', '....', '.#.#'], transformations)
-  //     ).toEqual(['##.##.', '#..#..', '......', '##.##.', '#..#..', '......']);
-  //   });
+  it('should getNextGrid', () => {
+    const transformations = parseInput(`../.# => ##./#../...
+.#./..#/### => #..#/..../..../#..#`);
+    expect(
+      getNextGrid(['....', '.#.#', '....', '.#.#'], transformations)
+    ).toEqual(['##.##.', '#..#..', '......', '##.##.', '#..#..', '......']);
+  });
 
   it('should get the value', done => {
     fs.readFile('inputs/day21.txt', 'utf8', (err, data) => {
@@ -27,13 +27,32 @@ describe('getNextGrid', () => {
 
       const grids = [initialGrid];
       while (grids.length < 6) {
-        console.log(`GRID ${grids.length - 1}`, grids[grids.length - 1]);
         grids.push(getNextGrid(grids[grids.length - 1], transformations));
       }
 
-      console.log(`GRID ${grids.length - 1}`, grids[grids.length - 1]);
-
       const finalGrid = grids[grids.length - 1];
+      expect(
+        finalGrid
+          .join('')
+          .split('')
+          .filter(a => a === '#').length
+      ).toMatchSnapshot();
+      done();
+    });
+  });
+
+  it.skip('should get the value', done => {
+    fs.readFile('inputs/day21.txt', 'utf8', (err, data) => {
+      const transformations = parseInput(data);
+
+      let iteration = 0;
+      let nextGrid = initialGrid;
+      while (iteration < 18) {
+        iteration++;
+        nextGrid = getNextGrid(nextGrid, transformations);
+      }
+
+      const finalGrid = nextGrid;
       expect(
         finalGrid
           .join('')
